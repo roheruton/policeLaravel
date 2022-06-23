ef57e98bb9ac5f9c8c1aa299e30c4a928895583b
roheruton <roheru.ton@gmail.com>
2022-06-23 15:12:24 -0400

    Update DetectingFaceController.php

--- app/Http/Controllers/Api/DetectingFaceController.php
+++ app/Http/Controllers/Api/DetectingFaceController.php
@@ -13,6 +13,7 @@ class DetectingFaceController extends Controller
     public function __construct()
     {
         $this->endPoint = "https://sw-evento.s3.us-west-2.amazonaws.com/";
+        $this->database = app('firebase.database');
     }
 
     public function index()
@@ -24,19 +25,43 @@ class DetectingFaceController extends Controller
         ////////////////////////////////////////////////
 
         error_log('Pruebaaa1');
-        $path = $request->file('image')->getPathname();
+      /*  $path = $request->file('image')->getPathname();
+        $path = $request->getPathname();
         $image= fopen($path,'r');
         $img = fread($image, filesize($path));
-
+        fclose($image);*/
+        $img =$this->getImage($request);
+       
         error_log('Pruebaaa2');
+        ///validar si en la foto recibida hay un rostro
+        $bounding="";
+        $faces=$this->validatePhotos($img);
+            if (count($faces['FaceDetails'])>1 ||count($faces['FaceDetails'])<=0 ) {
+                return response()->json(
+                    ["message" => "Foto inválida, es posible que haya mas de un rostro o ningún rostro"],
+                    Response::HTTP_BAD_REQUEST);
+            }
 
+        
         $resultado = $this->listFaces($img);
             error_log($resultado);
     /////////////////////////////////////////////
-        //if (count($resultado["Faces"]) > 0) {
-
-            return response()->json(["data" => $resultado['FaceMatches']]);
-        //}
+        if (count($resultado["FaceMatches"]) > 0) {
+            $policeID=$resultado['FaceMatches'][0]['Face']['ExternalImageId'];
+            $confidence= $resultado['FaceMatches'][0]['Face']['Confidence'];            
+            $bounding=  $faces['FaceDetails'][0]['BoundingBox'];
+            $policeInfo =$this->database
+                                ->getReference('police/'.$policeID)
+                                ->getValue();
+            $resultado= [
+                    'id'=>$policeID,
+                    'confidence'=>$confidence,
+                    'bounding' =>$bounding,
+                    'info'=>$policeInfo
+            ];
+                    
+            return response()->json(["data" => $resultado]);
+        }
         return response()->json(
             ["message" => "no se encontro"],
              Response::HTTP_BAD_REQUEST);
@@ -62,4 +87,45 @@ class DetectingFaceController extends Controller
 
         return $result;
     }
+
+    private function validatePhotos($photo)
+    {
+       // foreach ($photos as $photo) {
+            //$image = $this->getImage($photo);
+            $result = $this->faceDetected($photo);
+            if (count($result['FaceDetails']) > 1 || count($result['FaceDetails']) <= 0) {
+                return $result;
+            }
+            return $result;
+       // }
+    }
+
+    private function faceDetected($imageBase64)
+    {
+        $client = new RekognitionClient(
+            [
+                'region' => env('AWS_DEFAULT_REGION', 'us-west-2'),
+                'version' => 'latest'
+            ]
+        );
+        $result = $client->detectFaces([
+            'Attributes' => ['ALL'],
+            'Image' => [
+                'Bytes' => $imageBase64,
+            ],
+        ]);
+        return $result;
+    }
+    public function getImage($photo)
+    {
+        $imagePath =$photo-> file('image')->getPathname();
+        $fp_image = fopen($imagePath, 'r');
+        $image = fread($fp_image, filesize($imagePath));
+        fclose($fp_image);
+        return $image;
+    }
+
+
+
+
 }
