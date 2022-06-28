e72e7550163379b68b548138114681447472ede3
CORP\rsuarezl <Roheru>
2022-06-28 18:39:41 -0400

    adding api

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-//Route::post('/prueba', [DetectingFaceController::class, 'detectedFaceImage']);
+Route::post('/prueba5', [DetectingFaceController::class, 'detectedFaceImage']);
 //Route::post('/prueba', [DetectingFaceController::class, 'detectedFaceImage']);
