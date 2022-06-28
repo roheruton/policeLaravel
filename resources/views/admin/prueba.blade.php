195a52d9ad05fb3be606a3d7926ea85b01d3d284
CORP\rsuarezl <Roheru>
2022-06-28 09:28:10 -0400

    quit crfs in rueba.blade

--- resources/views/admin/prueba.blade.php
+++ resources/views/admin/prueba.blade.php
@@ -6,7 +6,7 @@
     <div class="card">
         <div class="card-body">
             <form action="{{ route('prueba.detectedFace') }}" method="post" enctype="multipart/form-data">
-                @csrf
+               
                 <label for="">Agrege una imagen: </label>
                 <input type="file" name="image" id="image" class="form-control form-control-sm">
                 <button type="submit" class="btn btn-sm btn-primary"> Guardar</button>
