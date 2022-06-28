f44e4dfa0213fc0d4a435e2f56feb37efe592c79
CORP\rsuarezl <Roheru>
2022-06-28 10:49:34 -0400

    keeping the form only at prueba.blade

--- resources/views/admin/prueba.blade.php
+++ resources/views/admin/prueba.blade.php
@@ -1,13 +1,8 @@
 
-
-    <div class="card">
-        <div class="card-body">
             <form action="{{ route('prueba.detectedFace') }}" method="post" enctype="multipart/form-data">
                
                 <label for="">Agrege una imagen: </label>
                 <input type="file" name="image" id="image" class="form-control form-control-sm">
                 <button type="submit" class="btn btn-sm btn-primary"> Guardar</button>
             </form>
-        </div>
-    </div>
 
