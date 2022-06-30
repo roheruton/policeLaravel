c59572c902536f96144c53010770a28cf26869c5
CORP\rsuarezl <Roheru>
2022-06-30 08:14:26 -0400

    updating default in order to allow max body size to 40 MB

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
 Route::post('/prueba2', [DetectingFaceController::class, 'detectedFaceImage2'])->name('prueba.detectedFace');
 Route::post('/prueba1', [DetectingFaceController::class, 'detectedFaceImage'])->name('prueba.detectedFace');
 //Route::post('/prueba', [DetectingFaceController::class, 'detectedFaceImage']);
